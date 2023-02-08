c5a5545e423a9d20999a960417699fc6a0f15292
Ahana Deb <ahanadeb01@gmail.com>
2023-02-08 03:53:01 +0100

    Add files via upload

---
+++ src/utils/transition.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def get_transitions(X, A, p, q_low, q_high):
+    P = np.zeros((X, X, A))
+    for i in range(0, X):
+        if i + 1 < X:
+            P[i, i + 1, 0] = p * (1 - q_low)  # 0 corresponds to low action
+            P[i, i + 1, 1] = p * (1 - q_high)  # 1 corresponds to high action
+        P[i, i, 0] = (1 - p) * (1 - q_low) + p * q_low
+        P[i, i, 1] = (1 - p) * (1 - q_high) + p * q_high
+        if i - 1 >= 0:
+            P[i, i - 1, 0] = q_low * (1 - p)  # 0 corresponds to low action
+            P[i, i - 1, 1] = q_high * (1 - p)  # 1 corresponds to high action
+    return P
