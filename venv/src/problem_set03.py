6f952a5ce8fd8b39884602885eb3e45312186cd8
Ahana Deb <ahanadeb01@gmail.com>
2023-03-15 14:39:15 +0100

    third commit

--- venv/src/problem_set03.py
+++ venv/src/problem_set03.py
@@ -17,7 +17,7 @@ from utils.soft_policy_iteration import *
 
 def problem_set03():
     F = pl_feature(X)
-    M = 100
+    M = 2
     eta_array = np.logspace(-2, +2, num=M)
     rewards = np.zeros((len(eta_array), 1))
     maxiter = 100
@@ -26,7 +26,12 @@ def problem_set03():
     x = np.arange(M)
     print(rewards)
     plt.semilogx(eta_array, rewards)
+    max_reward = np.argmax(rewards)
+    opt_eta = eta_array[max_reward]
+
     plt.ylabel('rewards gathered')
     plt.xlabel('eta values (log scale)')
+    plt.axvline(x=opt_eta, color='r')
+    plt.figtext(.6, .8, "optimal eta = "+ str(opt_eta))
     plt.show()
     return 0
